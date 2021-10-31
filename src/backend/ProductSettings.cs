f322848d639d2ae7d99accada328db866f1c5d23
Gubanov Ilya <yoyrandao@gmail.com>
2021-10-31 17:12:54 +0300

    feat: configured step for image publishing

--- src/backend/ProductSettings.cs
+++ src/backend/ProductSettings.cs
@@ -4,8 +4,6 @@ using System.Resources;
 
 
 [assembly: AssemblyCompany("Keebox")]
-[assembly: AssemblyVersion("0.0.*")]
-[assembly: AssemblyFileVersion("0.0.0.000")]
-[assembly: AssemblyInformationalVersion("0.0.0.000")]
+[assembly: AssemblyVersion("0.0")]
 [assembly: CLSCompliant(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
