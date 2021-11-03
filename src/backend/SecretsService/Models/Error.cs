d184581b10f2b68bee5b56ad2e2a2a5f7f91a596
Andrey Kiselev <omega-faworit@yandex.ru>
2021-11-04 00:31:17 +0300

    feat: add timestamp to error

--- src/backend/SecretsService/Models/Error.cs
+++ src/backend/SecretsService/Models/Error.cs
@@ -1,15 +1,20 @@
-﻿namespace Keebox.SecretsService.Models
+﻿using System;
+
+namespace Keebox.SecretsService.Models
 {
-	public record Error
-	{
-		public Error(int statusCode, string message)
-		{
-			StatusCode = statusCode;
-			Message = message;
-		}
+    public record Error
+    {
+        public Error(int statusCode, string message)
+        {
+            StatusCode = statusCode;
+            Message = message;
+            Timestamp = DateTime.Now;
+        }
 
-		public int StatusCode { init; get; }
+        public int StatusCode { init; get; }
 
-		public string Message { init; get; }
-	}
+        public string Message { init; get; }
+
+        public DateTime Timestamp { init; get; }
+    }
 }
