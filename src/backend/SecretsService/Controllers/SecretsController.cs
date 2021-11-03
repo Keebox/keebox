0d91e6005279666135a20dbc69bc7c67a0396642
Andrey Kiselev <omega-faworit@yandex.ru>
2021-11-04 00:26:39 +0300

    feat: add getting system info

--- src/backend/SecretsService/Controllers/SecretsController.cs
+++ src/backend/SecretsService/Controllers/SecretsController.cs
@@ -1,91 +1,93 @@
 ﻿using System;
 using System.Linq;
-
 using Keebox.Common.Managers;
 using Keebox.Common.Types;
 using Keebox.SecretsService.Models;
 using Keebox.SecretsService.RequestFiltering;
 using Keebox.SecretsService.Services;
-
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 
 namespace Keebox.SecretsService.Controllers
 {
-	[ApiController]
-	[Authenticate]
-	[Route("{*route}")]
-	public class SecretsController : ControllerBase
-	{
-		private readonly Configuration _configuration;
-		private readonly IFileConverter _fileConverter;
-		private readonly IFormatterResolver _formatterResolver;
-		private readonly ILogger<SecretsController> _logger;
+    [ApiController]
+    [Authenticate]
+    [Route("{*route}")]
+    public class SecretsController : ControllerBase
+    {
+        private readonly Configuration _configuration;
+        private readonly IFileConverter _fileConverter;
+        private readonly IFormatterResolver _formatterResolver;
+        private readonly ILogger<SecretsController> _logger;
 
-		private readonly ISecretsManager _secretsManager;
+        private readonly ISecretsManager _secretsManager;
 
-		public SecretsController(
-			ISecretsManager            secretsManager,
-			IFileConverter             fileConverter,
-			IFormatterResolver         formatterResolver,
-			Configuration              configuration,
-			ILogger<SecretsController> logger
-		)
-		{
-			_secretsManager = secretsManager;
-			_fileConverter = fileConverter;
-			_formatterResolver = formatterResolver;
-			_configuration = configuration;
-			_logger = logger;
-		}
+        public SecretsController(
+            ISecretsManager secretsManager,
+            IFileConverter fileConverter,
+            IFormatterResolver formatterResolver,
+            Configuration configuration,
+            ILogger<SecretsController> logger
+        )
+        {
+            _secretsManager = secretsManager;
+            _fileConverter = fileConverter;
+            _formatterResolver = formatterResolver;
+            _configuration = configuration;
+            _logger = logger;
+        }
 
-		[HttpPut]
-		public void AddSecrets([FromRoute] RequestPayload payload)
-		{
-			var secretNames = payload.Secrets?.Split(',') ?? Array.Empty<string>();
+        [HttpPut]
+        public void AddSecrets([FromRoute] RequestPayload payload)
+        {
+            _logger.LogDebug($"Adding secrets {payload.Route}");
+            var secretNames = payload.Secrets?.Split(',') ?? Array.Empty<string>();
 
-			if (payload.Data is null || !payload.Data.Keys.Any())
-				throw new Exception("Secrets are not provided");
+            if (payload.Data is null || !payload.Data.Keys.Any())
+                throw new Exception("Secrets are not provided");
 
-			_secretsManager.AddSecrets(payload.Route ?? string.Empty, payload.Data, _fileConverter.Convert(payload.Files), secretNames);
-		}
+            _secretsManager.AddSecrets(payload.Route ?? string.Empty, payload.Data,
+                _fileConverter.Convert(payload.Files), secretNames);
+        }
 
-		[HttpGet]
-		public string? GetSecrets([FromRoute] RequestPayload payload)
-		{
-			var secretNames = payload.Secrets?.Split(',') ?? Array.Empty<string>();
+        [HttpGet]
+        public string? GetSecrets([FromRoute] RequestPayload payload)
+        {
+            _logger.LogDebug($"Getting secrets {payload.Route}");
+            var secretNames = payload.Secrets?.Split(',') ?? Array.Empty<string>();
 
-			var secrets = _secretsManager.GetSecrets(payload.Route ?? string.Empty, secretNames);
+            var secrets = _secretsManager.GetSecrets(payload.Route ?? string.Empty, secretNames);
 
-			if (!payload.IncludeFiles)
-				secrets = secrets.Where(x => !x.IsFile);
+            if (!payload.IncludeFiles)
+                secrets = secrets.Where(x => !x.IsFile);
 
-			var format = payload.Format ?? _configuration.DefaultFormat;
+            var format = payload.Format ?? _configuration.DefaultFormat;
 
-			var formatter = _formatterResolver.Resolve(format);
-			HttpContext.Response.ContentType = ResolveContentType(format);
+            var formatter = _formatterResolver.Resolve(format);
+            HttpContext.Response.ContentType = ResolveContentType(format);
 
-			return formatter.Format(secrets);
-		}
+            return formatter.Format(secrets);
+        }
 
-		[HttpDelete]
-		public void DeleteSecrets([FromRoute] RequestPayload payload)
-		{
-			var secretNames = payload.Secrets?.Split(',') ?? Array.Empty<string>();
+        [HttpDelete]
+        public void DeleteSecrets([FromRoute] RequestPayload payload)
+        {
+            _logger.LogDebug($"Deleting secrets {payload.Route}");
+            var secretNames = payload.Secrets?.Split(',') ?? Array.Empty<string>();
 
-			_secretsManager.DeleteSecrets(payload.Route ?? string.Empty, secretNames);
-		}
+            _secretsManager.DeleteSecrets(payload.Route ?? string.Empty, secretNames);
+        }
 
-		private static string ResolveContentType(FormatType? format)
-		{
-			return format switch
-			{
-				FormatType.Env  => "text/plain",
-				FormatType.Json => "application/json",
-				FormatType.Xml  => "text/plain",
-				_               => "text/plain"
-			};
-		}
-	}
+        private static string ResolveContentType(FormatType? format)
+        {
+            return format switch
+            {
+                FormatType.Env => "text/plain",
+                FormatType.Json => "application/json",
+                FormatType.Xml => "text/plain",
+                _ => "text/plain"
+            };
+        }
+    }
 }
--- src/backend/SecretsService/Controllers/SystemController.cs
+++ src/backend/SecretsService/Controllers/SystemController.cs
@@ -1,46 +1,57 @@
-﻿using Keebox.SecretsService.Models;
-
+﻿using System;
+using Keebox.Common.Managers;
+using Keebox.Common.Types;
+using Keebox.SecretsService.Models;
 using Microsoft.AspNetCore.Mvc;
 
 
 namespace Keebox.SecretsService.Controllers
 {
-	[ApiController]
-	[Route("system")]
-	public class SystemController : ControllerBase
-	{
-		[HttpGet("status")]
-		[ProducesResponseType(200)]
-		public SystemInfo GetSystemInfo()
-		{
-			// TODO: add getting version and storage type
-			return new SystemInfo
-			{
-				Version = "0.0.0",
-				StorageType = "Inmemory"
-			};
-		}
+    [ApiController]
+    [Route("system")]
+    public class SystemController : ControllerBase
+    {
+        public SystemController(Configuration configuration)
+        {
+            _configuration = configuration;
+        }
 
-		[HttpGet("config")]
-		[ProducesResponseType(200)]
-		public Config GetConfig()
-		{
-			// TODO: add real config
-			return new Config();
-		}
+        [HttpGet("status")]
+        [ProducesResponseType(200)]
+        public SystemInfo GetSystemInfo()
+        {
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            var fileVersionInfo = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
+            var version = fileVersionInfo.ProductVersion ?? throw new InvalidOperationException();
+            return new SystemInfo
+            {
+                Version = version,
+                StorageType = _configuration.Engine.ToString()!
+            };
+        }
 
-		[HttpGet("start")]
-		[ProducesResponseType(200)]
-		public void Start()
-		{
-			// TODO: add start logic
-		}
+        [HttpGet("config")]
+        [ProducesResponseType(200)]
+        public Config GetConfig()
+        {
+            // TODO: add real config
+            return new Config();
+        }
 
-		[HttpGet("stop")]
-		[ProducesResponseType(200)]
-		public void Stop()
-		{
-			// TODO: add stop logic
-		}
-	}
+        [HttpGet("start")]
+        [ProducesResponseType(200)]
+        public void Start()
+        {
+            // TODO: add start logic
+        }
+
+        [HttpGet("stop")]
+        [ProducesResponseType(200)]
+        public void Stop()
+        {
+            // TODO: add stop logic
+        }
+
+        private readonly Configuration _configuration;
+    }
 }
