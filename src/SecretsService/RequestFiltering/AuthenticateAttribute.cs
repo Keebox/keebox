76c9e2b988ab6435678f2b0577bbf28912a64ac7
Andrey Kiselev <omega-faworit@yandex.ru>
2021-11-07 17:13:42 +0300

    feat: move files to src

--- src/SecretsService/RequestFiltering/AuthenticateAttribute.cs
+++ src/SecretsService/RequestFiltering/AuthenticateAttribute.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+
 using Keebox.Common.Exceptions;
 using Keebox.Common.Helpers;
 using Keebox.Common.Types;
+
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.DependencyInjection;
@@ -9,78 +11,77 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Keebox.SecretsService.RequestFiltering
 {
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public class AuthenticateAttribute : Attribute, IActionFilter
-    {
-        public void OnActionExecuting(ActionExecutingContext context)
-        {
-            var serviceProvider = context.HttpContext.RequestServices;
+	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+	public class AuthenticateAttribute : Attribute, IActionFilter
+	{
+		public void OnActionExecuting(ActionExecutingContext context)
+		{
+			var serviceProvider = context.HttpContext.RequestServices;
 
-            var tokenService = serviceProvider.GetRequiredService<ITokenValidator>();
-            var configuration = serviceProvider.GetRequiredService<Configuration>();
+			var tokenService = serviceProvider.GetRequiredService<ITokenValidator>();
+			var configuration = serviceProvider.GetRequiredService<Configuration>();
 
-            var authenticationType = ResolveAuthenticationType(context.HttpContext);
+			var authenticationType = ResolveAuthenticationType(context.HttpContext);
 
-            switch (authenticationType)
-            {
-                case AuthenticationType.Token:
-                {
-                    var token = context.HttpContext.Request.Headers[TokenHeader];
+			switch (authenticationType)
+			{
+				case AuthenticationType.Token:
+				{
+					var token = context.HttpContext.Request.Headers[TokenHeader];
 
-                    if (configuration.RootToken!.Equals(token, StringComparison.OrdinalIgnoreCase))
-                        return;
+					if (configuration.RootToken!.Equals(token, StringComparison.OrdinalIgnoreCase))
+						return;
 
-                    if (context.HttpContext.Request.Path.ToString().StartsWith("/account"))
-                        throw new RestrictedAccessException("Account management only for admins");
+					if (context.HttpContext.Request.Path.ToString().StartsWith("/account"))
+						throw new RestrictedAccessException("Account management only for admins");
 
-                    if (!tokenService.Validate(token))
-                        throw new UnauthorizedException("Token is not valid.");
+					if (!tokenService.Validate(token))
+						throw new UnauthorizedException("Token is not valid.");
 
-                    break;
-                }
-                case AuthenticationType.Cookie:
-                {
-                    var token = context.HttpContext.Request.Cookies[TokenHeader];
+					break;
+				}
+				case AuthenticationType.Cookie:
+				{
+					var token = context.HttpContext.Request.Cookies[TokenHeader];
 
-                    if (configuration.RootToken!.Equals(token, StringComparison.OrdinalIgnoreCase))
-                        return;
+					if (configuration.RootToken!.Equals(token, StringComparison.OrdinalIgnoreCase))
+						return;
 
-                    if (context.HttpContext.Request.Path.ToString().StartsWith("/account"))
-                        throw new RestrictedAccessException("Account management only for admins");
+					if (context.HttpContext.Request.Path.ToString().StartsWith("/account"))
+						throw new RestrictedAccessException("Account management only for admins");
 
-                    if (!tokenService.Validate(token))
-                        throw new UnauthorizedException("Token is not valid.");
+					if (!tokenService.Validate(token))
+						throw new UnauthorizedException("Token is not valid.");
 
-                    break;
-                }
-                case AuthenticationType.None:
-                    throw new UnauthorizedException("Authentication was not successful");
+					break;
+				}
+				case AuthenticationType.None:
+					throw new UnauthorizedException("Authentication was not successful");
 
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
-        }
+				case AuthenticationType.Certificate:
+				default:
+					throw new ArgumentOutOfRangeException();
+			}
+		}
 
-        public void OnActionExecuted(ActionExecutedContext context)
-        {
-        }
+		public void OnActionExecuted(ActionExecutedContext context) { }
 
-        private static AuthenticationType ResolveAuthenticationType(HttpContext context)
-        {
-            if (context.Connection.ClientCertificate != null)
-                return AuthenticationType.Certificate;
+		private static AuthenticationType ResolveAuthenticationType(HttpContext context)
+		{
+			if (context.Connection.ClientCertificate != null)
+				return AuthenticationType.Certificate;
 
-            if (context.Request.Headers.ContainsKey(TokenHeader))
-                return AuthenticationType.Token;
+			if (context.Request.Headers.ContainsKey(TokenHeader))
+				return AuthenticationType.Token;
 
-            if (context.Request.Cookies.ContainsKey(TokenHeader))
-            {
-                return AuthenticationType.Cookie;
-            }
+			if (context.Request.Cookies.ContainsKey(TokenHeader))
+			{
+				return AuthenticationType.Cookie;
+			}
 
-            return AuthenticationType.None;
-        }
+			return AuthenticationType.None;
+		}
 
-        private const string TokenHeader = "X-Token";
-    }
+		private const string TokenHeader = "X-Token";
+	}
 }
