af5f382ed0a3b9ab4f2feb8021ba8d31e700f822
Andrey Kiselev <omega-faworit@yandex.ru>
2021-11-14 00:02:12 +0300

    feat: add response type

--- src/SecretsService/Controllers/RoleController.cs
+++ src/SecretsService/Controllers/RoleController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
 
 using Keebox.Common.DataAccess.Entities;
 using Keebox.Common.Managers;
 using Keebox.SecretsService.RequestFiltering;
 
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 
@@ -21,30 +23,47 @@ namespace Keebox.SecretsService.Controllers
 		}
 
 		[HttpGet("{roleId:guid}")]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
 		public Role GetRole([FromQuery] Guid roleId)
 		{
 			return _roleManager.GetRole(roleId);
 		}
-		
+
 		[HttpGet]
+		[ProducesResponseType(StatusCodes.Status200OK)]
 		public IEnumerable<Role> ListRoles()
 		{
 			return _roleManager.GetRoles();
 		}
 
 		[HttpPost]
+		[ProducesResponseType(StatusCodes.Status201Created)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		[ProducesResponseType(StatusCodes.Status409Conflict)]
 		public Guid CreateRole([FromBody] string name)
 		{
 			return _roleManager.CreateRole(name);
 		}
 
 		[HttpPut("{roleId:guid}")]
-		public void ReplaceRole([FromBody] Role role)
+		[ProducesResponseType(StatusCodes.Status204NoContent)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		public void ReplaceRole([FromBody] Role role, [FromQuery] Guid roleId)
 		{
+			if (roleId != role.Id)
+			{
+				throw new ArgumentException("Ids do not match");
+			}
+
 			_roleManager.UpdateRole(role);
 		}
 
 		[HttpDelete("{roleId:guid}")]
+		[ProducesResponseType(StatusCodes.Status204NoContent)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
 		public void DeleteRole([FromQuery] Guid roleId)
 		{
 			_roleManager.DeleteRole(roleId);
--- src/SecretsService/RequestFiltering/ExceptionsFilter.cs
+++ src/SecretsService/RequestFiltering/ExceptionsFilter.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Net;
 
 using Keebox.Common.Exceptions;
@@ -27,6 +28,11 @@ namespace Keebox.SecretsService.RequestFiltering
 
 			switch (context.Exception)
 			{
+				case ArgumentException:
+					status = HttpStatusCode.BadRequest;
+					message = context.Exception.Message;
+
+					break;
 				case NotFoundException:
 					status = HttpStatusCode.NotFound;
 					message = context.Exception.Message;
